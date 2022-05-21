541f3cf1a982bb7eb35b441a5b6a80f6d2ef8da9
Hammad538 <hammad.rashid@devsinc.com>
2022-05-22 01:20:30 +0500

    *Stripe: Resolved error of stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 require 'stripe'
 
@@ -7,11 +6,3 @@ Rails.configuration.stripe = {
   secret_key: ENV['API_KEY'],
   webhook: ENV['WEBHOOK']
 }
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
-StripeEvent.signing_secret = Rails.application.credentials.stripe[:webhook]
-StripeEvent.configure do |events|
-  events.subscribe 'invoice.paid' do |event|
-    InvoiceMailer.with({ "customer_email": event.data.object.customer_email }).order_successful_email.deliver_now
-  end
-end
