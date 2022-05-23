debd6b374ec05d10c230920f5272a035d51b41f2
Hammad538 <hammad.rashid@devsinc.com>
2022-05-23 20:38:14 +0500

    *Transactions: Admin can View All transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  resources :transactions
   resources :features do
     resources :featureusages
   end
   resources :plans
   resources :buyers
-                      resources :admins
+  resources :admins
   resources :subscriptions
   devise_for :users
- resources :webhooks
+  resources :webhooks
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
