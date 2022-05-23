280dc4734b5d0aea645b43543cf4354a5c53c791
Hammad538 <hammad.rashid@devsinc.com>
2022-05-23 18:02:38 +0500

    *UI-Admin-Pannel: Implemented Bootstrap on admin pannel

--- app/helpers/buyers_helper.rb
+++ app/helpers/buyers_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module BuyersHelper
+  def check_subscription_existence(plan_id)
+    find_result=current_user.subscriptions.where(is_active:true).find_by(plan_id:"#{plan_id}").present? && current_user.type=="Buyer"
+  end
+  def find_subscription(plan_id)
+    subscription=current_user.subscriptions.where(is_active:true).find_by(plan_id:"#{plan_id}")
+  end
+
 end
