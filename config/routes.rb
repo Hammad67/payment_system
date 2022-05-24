084701380cb19ebde02253a7fa38631fbdb46a2e
Hammad538 <hammad.rashid@devsinc.com>
2022-05-24 23:04:51 +0500

    *Routes:Cleaned all restfull  routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   resources :features do
     resources :featureusages, except: [:destroy]
   end
+
   resources :plans
   resources :buyers
-  resources :admins, only: [:index]
+  resources :admins, only: %i[index]
   resources :subscriptions, only: %i[new create show]
-  devise_for :users, skip: [:registrations]
-  resources :webhooks, only: [:create]
+  devise_for :users, skip: %i[registrations]
+  resources :webhooks, only: %i[create]
+
   root to: 'homes#index'
 end
