33407f7ddbece85e31888ac512e784db18e533ea
Hammad538 <hammad.rashid@devsinc.com>
2022-05-25 10:36:38 +0500

    Bugs: Resplved all bugs from app

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -3,6 +3,6 @@ class Feature < ApplicationRecord
   validates :name, :code, :unit_price, :max_unit_limit, presence: true
   validates :unit_price, numericality: true
   validates :max_unit_limit, numericality: { greater_than: :unit_price }
-  validates :name, :code, uniqueness: true, length: { minimum: 10, maximum: 30 }
+  validates :name, :code, uniqueness: true, length: { minimum: 10, maximum: 20 }
   has_many :featureusages, dependent: :destroy
 end
