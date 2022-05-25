d03cfc4d2dd9962f7470cce5dbc3e12f5afcee65
Hammad538 <hammad.rashid@devsinc.com>
2022-05-25 10:16:02 +0500

    *Error: Resolved error of migrations

--- db/migrate/20220524182726_add_constraints_in_to_tables.rb
+++ db/migrate/20220524182726_add_constraints_in_to_tables.rb
@@ -1,6 +1,6 @@
 class AddConstraintsInToTables < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :name, :string, limit: 10
+    change_column :users, :name, :string, limit: 20
     change_column :plans, :name, :string, limit: 30
     change_column :features, :name, :string, limit: 30
     change_column :features, :code, :string, limit: 20
